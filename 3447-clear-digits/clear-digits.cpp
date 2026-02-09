8dc1782630f1ef589331645377aa75ec76244061
Saurav Deep <149681554+Sauravdeep01@users.noreply.github.com>
2026-02-10 00:58:18 +0530

    Time: 1 ms (23.61%) | Memory: 9.1 MB (22.40%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+
+        for(int i=0; i<s.length(); i++){
+            if(s[i] >= '0' && s[i] <= '9'){
+                if(!st.empty()){
+                    st.pop();
+                }
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+
+        string ans = "";
+        while(!st.empty()){
+            ans = st.top() + ans;
+            st.pop();
+        }
+
+        return ans;
+        
+    }
+};
