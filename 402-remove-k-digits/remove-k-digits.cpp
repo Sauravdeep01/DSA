aba57fa79476751961373ecbfe00c2a89b5db655
Saurav Deep <149681554+Sauravdeep01@users.noreply.github.com>
2026-02-11 14:21:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (85.70%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -2,41 +2,35 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
 
-        stack<char> st;
-        for(int i=0; i<num.length(); i++){
+        string st = "";
+
+        for(int i = 0; i < num.length(); i++) {
             char c = num[i];
 
-            while(!st.empty() && k>0 && st.top() > c){
-                st.pop();
+            while(!st.empty() && k > 0 && st.back() > c) {
+                st.pop_back();
                 k--;
             }
-            st.push(c);
 
-        }
-        while(k>0 && !st.empty()){
-                st.pop();
-                k--;
+            st.push_back(c);
         }
 
-        string ans="";
-        while(!st.empty()){
-            ans += st.top();
-            st.pop();
-        }   
+        // If k still left
+        while(k > 0 && !st.empty()) {
+            st.pop_back();
+            k--;
+        }
 
-        reverse(ans.begin(), ans.end());
-
-        int i=0;
-        while(i<ans.length() && ans[i] == '0'){
+        // Remove leading zeros
+        int i = 0;
+        while(i < st.length() && st[i] == '0') {
             i++;
         }
 
-        ans = ans.substr(i);
-        if(ans == ""){
-            return "0";
-        }
+        st = st.substr(i);
 
-        return ans;
-        
+        if(st == "") return "0";
+
+        return st;
     }
 };
