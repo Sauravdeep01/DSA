42897a145fb3ae814fbcadf0b64e6c672b24144b
Saurav Deep <149681554+Sauravdeep01@users.noreply.github.com>
2026-03-08 18:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (93.53%) - LeetSync

---
+++ 2168-check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool areNumbersAscending(string s) {
+        int prev = 0;
+        int n = s.length();
+        
+        for(int i = 0; i < n; i++){
+            if(isdigit(s[i])){
+                int num = 0;
+                
+                while(i < n && isdigit(s[i])){
+                    num = num * 10 + (s[i] - '0');
+                    i++;
+                }
+                
+                if(num <= prev)
+                    return false;
+                
+                prev = num;
+            }
+        }
+        
+        return true;
+    }
+};
