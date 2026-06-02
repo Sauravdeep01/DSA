c32535f302eca03ffedd00e45e6499e8abacea21
Saurav Deep <149681554+Sauravdeep01@users.noreply.github.com>
2026-06-02 23:56:23 +0530

    Time: 0 ms (100.00%) | Memory: 32.3 MB (57.73%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
+
+        vector<int> missing;
         
         sort(nums.begin(), nums.end());
 
-        vector<int>missing;
-
-        for(int i=0; i<nums.size()-1; i++) {
+        for(int i=0; i<nums.size()-1; i++){
             int current = nums[i];
             int next = nums[i+1];
 
-            for(int num = current+1; num < next; num++) {
+            for(int num = current + 1; num<next; num++){
                 missing.push_back(num);
             }
         }
